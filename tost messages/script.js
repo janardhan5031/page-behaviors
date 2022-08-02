3fe1aa2c984af3790620da13c340ae61ac1c131e
bjsaimani <45102237+bjsaimani@users.noreply.github.com>
2022-08-02 09:55:55 +0530

    tost msge

--- tost messages/script.js
+++ tost messages/script.js
@@ -2,6 +2,7 @@ const btn=document.getElementById('btn');
 const container=document.getElementById('container');
 
 btn.addEventListener('click',()=>{
+    console.log('clicked');
     const ele=document.createElement('div');
     ele.innerHTML='this is an error!';
     container.appendChild(ele);
