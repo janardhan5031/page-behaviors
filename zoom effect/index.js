7521e0d4289752cfc939d1967bfeefe5628824b2
bjsaimani <45102237+bjsaimani@users.noreply.github.com>
2022-08-21 19:55:11 +0530

    image zooimg efferect

---
+++ zoom effect/index.js
@@ -0,0 +1,19 @@
+//
+const container=document.getElementById('image-container');
+
+const image=document.querySelector('img');
+
+container.addEventListener('mousemove',(e) => {
+    const x=e.clientX-e.target.offsetLeft;
+    const y=e.clientY-e.target.offsetTop;
+
+    //console.log(x,y);
+
+    image.style.transformOrigin=`${x}px ${y}px`;
+    image.style.transform ="scale(3)" ;
+});
+
+container.addEventListener('mouseleave',()=> {
+    image.style.transformOrigin='center center';
+    image.style.transform ='scale(1)';
+})
